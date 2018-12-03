3b8f5f69f7d63cc8d116b6f2b5dec6da7ec69643
Roger Taylor <roger.taylor.email@gmail.com>
2018-12-04 08:34:01 +1300

    Electron Cash wallet copying: filter out weird ;p MacOS and Linux stuff like directories and hidden files.

--- electrumsv/gui/qt/installwizard.py
+++ electrumsv/gui/qt/installwizard.py
@@ -154,23 +154,30 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
         self.refresh_gui()  # Need for QT on MacOSX.  Lame.
 
     def run_and_get_wallet(self):
-        ret = self._copy_electron_cash_wallets()    
+        ret = self._copy_electron_cash_wallets()
         # Whether the user exited the application.
         if ret == -1:
             return
 
         # Otherwise, move onto the wallet selection.
         return self._run_and_get_wallet()
-        
+
     def _copy_electron_cash_wallets(self):
         """
         Work out whether we should show UI to offer to copy the user's
         Electron Cash wallets to their Electrum SV wallet directory, and
         if so, show it and give them the chance.
         """
+        def ignore_wallet_file(wallet_path):
+            if os.path.isdir(wallet_path):
+                return True
+            if wallet_path.startswith("."):
+                return True
+            return False
+
         def count_user_wallets(wallets_path):
             if os.path.exists(wallets_path):
-                filenames = os.listdir(wallets_path)
+                filenames = [ filename for filename in os.listdir(wallets_path) if not ignore_wallet_file(os.path.join(wallets_path, filename)) ]
                 return len(filenames)
             return 0
 
@@ -192,15 +199,16 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
                 summary_label.setText(_("1 wallet is selected / will be copied."))
             else:
                 summary_label.setText(_("%d wallets are selected / will be copied.") % selection_count)
-            
+
         wallet_filenames = sorted(os.listdir(ec_wallets_dir), key=lambda s: s.lower())
-        
+
         file_list = QListWidget()
         file_list.setSelectionMode(QAbstractItemView.ExtendedSelection)
         for filename in wallet_filenames:
-            file_list.addItem(QListWidgetItem(filename))
+            if not ignore_wallet_file(os.path.join(ec_wallets_dir, filename)):
+                file_list.addItem(QListWidgetItem(filename))
         file_list.itemSelectionChanged.connect(update_summary_label)
-        
+
         vbox = QVBoxLayout()
         introduction_label = QLabel(_("Your Electron Cash wallet directory was found. If you want Electrum SV to import any of them on your behalf, select the ones you want copied from the list below before clicking the Next button."))
         introduction_label.setWordWrap(True)
@@ -219,7 +227,7 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
             if v != 2:  # 2 = next
                 # If the user is not selecting close or next, I do not know what they are selecting. Cancel?
                 return -1
-            
+
             # If the user selected any files, then we copy them before exiting to the next page.
             for item in file_list.selectedItems():
                 filename = item.text()
@@ -231,7 +239,7 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
                     # For now we ignore copy errors.
                     pass
             break
-        
+
     def _run_and_get_wallet(self):
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
@@ -414,7 +422,7 @@ class InstallWizard(QDialog, MessageBoxMixin, BaseWizard):
 
     def remove_from_recently_open(self, filename):
         self.config.remove_from_recently_open(filename)
-        
+
     def text_input(self, title, message, is_valid, allow_multi=False):
         slayout = KeysLayout(parent=self, title=message, is_valid=is_valid,
                              allow_multi=allow_multi)
