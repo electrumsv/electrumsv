3484454191570ad34e403fe69947a3c1ca5f0db7
Roger Taylor <roger.taylor.email@gmail.com>
2019-01-02 21:18:50 +1300

    pylint: Add further non-noisy and hopefully helpful checks.

--- electrumsv/wallet.py
+++ electrumsv/wallet.py
@@ -915,7 +915,7 @@ class Abstract_Wallet:
 
     def get_label(self, tx_hash):
         label = self.labels.get(tx_hash, '')
-        if label is '':
+        if label == '':
             label = self.get_default_label(tx_hash)
         return label
 
