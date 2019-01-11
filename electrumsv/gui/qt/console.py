43b6a74af58152d49c4d0d8ab482fe5c34253ff5
Neil Booth <kyuupichan@gmail.com>
2019-01-11 22:36:34 +0800

    Add copyright; clean up imports

--- electrumsv/gui/qt/console.py
+++ electrumsv/gui/qt/console.py
@@ -1,12 +1,37 @@
+# ElectrumSV - lightweight BitcoinSV client
+# Copyright (C) 2012 thomasv@gitorious
+# Copyright (C) 2019 The Electrum SV Developers
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # source: http://stackoverflow.com/questions/2758159
 
 import logging
-import sys, os, re
-import traceback, platform
-from PyQt5 import QtCore
-from PyQt5 import QtGui
-from PyQt5 import QtWidgets
+import os
+import platform
+import re
+import sys
+import traceback
+
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 from electrumsv import util
 from electrumsv.i18n import _
