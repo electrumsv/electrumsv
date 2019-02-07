dfcef34de45139b31683f5bcdc40b3adc6b5464f
Roger Taylor <roger.taylor.email@gmail.com>
2019-02-07 17:16:21 +1300

    Prepare for 1.1.0 release. A few small things need to be done before the tag, but this is the version bump.

--- electrumsv/version.py
+++ electrumsv/version.py
@@ -1,6 +1,6 @@
-PACKAGE_VERSION = '1.1.0b1'                         # version of the client package
-PACKAGE_DATE = '2019-01-27T02:50:45.720171+00:00'   # official timestamp for client package
-PROTOCOL_VERSION = '1.4'                            # protocol version requested
+PACKAGE_VERSION = '1.1.0'                          # version of the client package
+PACKAGE_DATE = '2019-02-07T17:13:00.000000+13:00'  # official timestamp for client package
+PROTOCOL_VERSION = '1.4'                           # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
 SEED_PREFIX      = '01'      # Standard wallet
