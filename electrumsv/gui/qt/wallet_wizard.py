755d9c53cb27863e781a66f57f108755051dad12
rt121212121 <roger.taylor.email@gmail.com>
2019-03-13 15:05:09 +1300

    Wallet wizard wip commit.

--- electrumsv/gui/qt/wallet_wizard.py
+++ electrumsv/gui/qt/wallet_wizard.py
@@ -1,12 +1,22 @@
+# TODO:
+# - QTableWidget vs. QListView
+#
+#   Consolidate the use of QTableWidget and QListView between the wallet selection screen and
+#   the add wallet screen. The decision holding the choice back, is the need to both sort and
+#   filter the contents. I suspect that the choice may not matter though and we should go with
+#   one or the other.
+#
+# - ...
+
 import enum
 import os
 
-from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtCore import QSize, Qt, QModelIndex
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (
     QDialog, QVBoxLayout, QTableWidget, QAbstractItemView, QWidget,
     QHBoxLayout, QLabel, QMessageBox, QWizard, QWizardPage, QListWidget,
-    QListWidgetItem
+    QListWidgetItem, QHeaderView
 )
 
 from electrumsv.app_state import app_state
@@ -63,7 +73,18 @@ def open_wallet_wizard():
 
 class Pages(enum.IntEnum):
     SELECT_WALLET = 1
-    ADD_WALLET = 2
+    ADD_WALLET_MENU = 2
+    CREATE_NEW_STANDARD_WALLET = 3
+    CREATE_NEW_MULTISIG_WALLET = 4
+    IMPORT_WALLET_FILE = 5
+    IMPORT_WALLET_TEXT = 6
+    IMPORT_WALLET_TEXT_SEED_PHRASE_CENTBEE = 41
+    IMPORT_WALLET_TEXT_SEED_PHRASE_HANDCASH = 42
+    IMPORT_WALLET_TEXT_SEED_PHRASE_MONEYBUTTON = 43
+    IMPORT_HARDWARE_WALLET_FOR_DIGITALBITBOX = 51
+    IMPORT_HARDWARE_WALLET_FOR_KEEPKEY = 52
+    IMPORT_HARDWARE_WALLET_FOR_LEDGER = 53
+    IMPORT_HARDWARE_WALLET_FOR_TREZOR = 54
 
 
 class WalletWizard2(QWizard):
@@ -78,7 +99,7 @@ class WalletWizard2(QWizard):
 
         self.setOption(QWizard.HaveHelpButton, True)
         self.setPage(Pages.SELECT_WALLET, SelectWalletWizardPage(self))
-        self.setPage(Pages.ADD_WALLET, AddWalletWizardPage(self))
+        self.setPage(Pages.ADD_WALLET_MENU, AddWalletWizardPage(self))
 
         self.setStartId(Pages.SELECT_WALLET)
 
@@ -100,8 +121,12 @@ class WalletWizard2(QWizard):
         super().accept()
 
 
+wallet_table = None
+
 class SelectWalletWizardPage(QWizardPage):
     def __init__(self, parent):
+        global wallet_table
+
         super().__init__(parent)
 
         self.setTitle(_("Select Wallet"))
@@ -111,15 +136,15 @@ class SelectWalletWizardPage(QWizardPage):
         wallet_data = self._get_recently_opened_wallets()
 
         locked_pixmap = QPixmap(icon_path("icons8-lock-80.png")).scaledToWidth(
-            50, Qt.SmoothTransformation)
+            40, Qt.SmoothTransformation)
         unlocked_pixmap = QPixmap(icon_path("icons8-unlock-80.png")).scaledToWidth(
-            50, Qt.SmoothTransformation)
+            40, Qt.SmoothTransformation)
 
         layout = QVBoxLayout()
 
         wallet_table = self.wallet_table = QTableWidget()
         wallet_table.setColumnCount(1)
-        wallet_table.setIconSize(QSize(64, 64))
+        #wallet_table.setIconSize(QSize(24, 24))
         wallet_table.setHorizontalHeaderLabels([ "Recently Opened Wallets" ])
         wallet_table.setSelectionBehavior(QAbstractItemView.SelectRows)
         wallet_table.setStyleSheet("""
@@ -130,14 +155,15 @@ class SelectWalletWizardPage(QWizardPage):
                 font-weight: bold;
             }
         """)
+        # Tab by default in QTableWidget, moves between list items. The arrow keys also perform
+        # the same function, and we want tab to allow toggling to the wizard button bar instead.
         wallet_table.setTabKeyNavigation(False)
 
         hh = wallet_table.horizontalHeader()
         hh.setStretchLastSection(True)
 
         vh = wallet_table.verticalHeader()
-        vh.setSectionResizeMode(vh.Fixed)
-        vh.setDefaultSectionSize(80)
+        vh.setSectionResizeMode(QHeaderView.ResizeToContents)
         vh.hide()
 
         for d in wallet_data:
@@ -149,6 +175,8 @@ class SelectWalletWizardPage(QWizardPage):
 
             row_widget = QWidget()
             row_layout = QHBoxLayout()
+            row_layout.setSpacing(0)
+            row_layout.setContentsMargins(0, 0, 0, 0)
 
             row_icon_label = QLabel()
             if wallet_storage.is_encrypted():
@@ -176,7 +204,7 @@ class SelectWalletWizardPage(QWizardPage):
         wallet_table.selectRow(0)
 
     def nextId(self):
-        return Pages.ADD_WALLET
+        return Pages.ADD_WALLET_MENU
 
     def get_wallet_data(self):
         selected_indexes = self.wallet_table.selectedIndexes()
@@ -199,19 +227,15 @@ class SelectWalletWizardPage(QWizardPage):
         ]
 
 
-class AddEntryType(enum.IntEnum):
-    CREATE_NEW = 1
-    EXTERNAL_SEED_IMPORT = 2
-
-
 class AddWalletWizardPage(QWizardPage):
     def __init__(self, parent):
         super().__init__(parent)
 
         self.setTitle(_("Add Wallet"))
+        self.setFinalPage(False)
 
         option_list = self.option_list = QListWidget()
-        option_list.setIconSize(QSize(50, 50))
+        option_list.setIconSize(QSize(30, 30))
         option_list.setStyleSheet("""
             QListView::item:selected {
                 background-color: #F5F8FA;
@@ -219,42 +243,214 @@ class AddWalletWizardPage(QWizardPage):
             }
         """)
 
-        entries = [
-            {
-                'type': AddEntryType.CREATE_NEW,
-                'description': _("Create new wallet"),
-                'icon_filename': 'icons8-reuse-80.png',
-            },
-            {
-                'type': AddEntryType.EXTERNAL_SEED_IMPORT,
-                'description': _("Import any seed phrase with guessing"),
-                'icon_filename': 'icons8-brain-80.png',
-            },
-            {
-                'type': AddEntryType.EXTERNAL_SEED_IMPORT,
-                'description': _("Import using Centbee seed phrase"),
-                'icon_filename': 'icons8-reuse-80.png',
-            },
-            {
-                'type': AddEntryType.EXTERNAL_SEED_IMPORT,
-                'description': _("Import using HandCash seed phrase"),
-                'icon_filename': 'icons8-reuse-80.png',
-            },
-            {
-                'type': AddEntryType.EXTERNAL_SEED_IMPORT,
-                'description': _("Import using MoneyButton seed phrase"),
-                'icon_filename': 'icons8-reuse-80.png',
-            },
-        ]
-
-        for entry in entries:
+        for entry in self._get_entries():
             list_item = QListWidgetItem()
-            list_item.setSizeHint(QSize(100, 80))
+            list_item.setSizeHint(QSize(40, 40))
             list_item.setIcon(read_QIcon(entry['icon_filename']))
             list_item.setText(entry['description'])
+            list_item.setData(Qt.UserRole, entry)
             option_list.addItem(list_item)
 
-        layout = QVBoxLayout()
+        option_list.setMaximumWidth(400)
+        option_list.setWordWrap(True)
+
+        option_detail = self.optionDetail = QLabel()
+        option_detail.setMinimumWidth(200)
+        option_detail.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        option_detail.setTextFormat(Qt.RichText)
+        option_detail.setWordWrap(True)
+        option_detail.setOpenExternalLinks(True)
+        option_detail.setText(self._get_entry_detail())
+
+        layout = QHBoxLayout()
+        # It looks tidier with more separation between the list and the detail pane.
+        layout.setSpacing(15)
         layout.addWidget(option_list)
+        layout.addWidget(option_detail)
         self.setLayout(layout)
 
+        def _on_current_row_changed(current_row):
+            self.completeChanged.emit()
+
+        def _on_item_selection_changed():
+            self.completeChanged.emit()
+            selected_items = self.option_list.selectedItems()
+            if len(selected_items):
+                entry = selected_items[0].data(Qt.UserRole)
+                self.optionDetail.setText(self._get_entry_detail(entry))
+            else:
+                self.optionDetail.setText(self._get_entry_detail())
+
+        option_list.currentRowChanged.connect(_on_current_row_changed)
+        option_list.itemSelectionChanged.connect(_on_item_selection_changed)
+
+    def isFinalPage(self):
+        return False
+
+    def isComplete(self):
+        selected_items = self.option_list.selectedItems()
+        return len(selected_items)
+
+    def validatePage(self):
+        selected_items = self.option_list.selectedItems()
+        return len(selected_items)
+
+    def nextId(self):
+        selected_items = self.option_list.selectedItems()
+        if len(selected_items):
+            return selected_items[0].data(Qt.UserRole)['page']
+        # If -1 is returned, it guesses final page and uses "Finish" instead
+        # of "Next".
+        return Pages.CREATE_NEW_STANDARD_WALLET
+
+    def _get_entry_detail(self, entry=None):
+        title_start_html = "<b>"
+        title_end_html = "</b>"
+        if entry is None:
+            title_start_html = title_end_html = ""
+            entry = {
+                'icon_filename': 'icons8-decision-80.png',
+                'description': _("Select the way in which you want to add a new wallet "+
+                    "from the list on the left."),
+            }
+        html = f"""
+        <center>
+            <img src='{icon_path(entry['icon_filename'])}' width=80 height=80 />
+        </center>
+        <p>
+            {title_start_html}
+            {entry['description']}
+            {title_end_html}
+        </p>
+        """
+        html += entry.get('long_description', '')
+        return html
+
+    def _get_entries(self):
+        seed_phrase_html = ("<p>"+
+            _("A seed phrase is a way of storing a wallet's private key. "+
+              "Using it ElectrumSV can access the wallet's previous "+
+              "payments, and send and receive the coins in the wallet.") +
+            "</p>")
+
+        original_wallet_html = ("<p>"+
+            _("If the original wallet application a seed phrase came from is still being used to "+
+              "access the given wallet, then it is not always safe to access it in ElectrumSV "+
+              "while this is the case.") +
+            " %(extra)s"+
+            "</p>")
+
+        original_wallet_unsafe_html = original_wallet_html % {
+            "extra": _("%(wallet_name)s is one of these applications. If you are still using it, "+
+                       "it may get confused if you do more than watch, like sending and "+
+                       "receiving coins using ElectrumSV."),
+        }
+
+        original_wallet_safe_html = original_wallet_html % {
+            "extra": _("%(wallet_name)s however, works in a compatible way "+
+                       "where it should be possible to use both it and "+
+                       "ElectrumSV at the same time."),
+        }
+
+        return [
+            {
+                'page': Pages.CREATE_NEW_STANDARD_WALLET,
+                'description': _("Create new standard wallet"),
+                'icon_filename': 'icons8-create-80.png',
+                'long_description': f"""
+                If you want to create a brand new wallet in ElectrumSV this is the option you
+                want.
+                """,
+            },
+            {
+                'page': Pages.CREATE_NEW_MULTISIG_WALLET,
+                'description': _("Create new multi-signature wallet"),
+                'icon_filename': 'icons8-create-80.png',
+                'long_description': f"""
+                If you want to create a brand new multi-signature wallet in ElectrumSV this is the
+                option you want.
+                """,
+            },
+            {
+                'page': Pages.IMPORT_WALLET_FILE,
+                'description': _("Import wallet file"),
+                'icon_filename': 'icons8-document.svg',
+                'long_description': f"""
+                ...
+                """,
+            },
+            {
+                'page': Pages.IMPORT_WALLET_TEXT,
+                'description': _("Import wallet using text (any seed phrase, public keys, "+
+                                 "private keys or addresses)"),
+                'icon_filename': 'icons8-brain-80.png',
+                'long_description': f"""
+                If you have some text to paste, or type in, and want ElectrumSV to examine it
+                and offer you some choices on how it can be imported, this is the option you
+                probably want.
+                """,
+            },
+            {
+                'page': Pages.IMPORT_WALLET_TEXT_SEED_PHRASE_CENTBEE,
+                'description': _("Import wallet using seed phrase (Centbee)"),
+                'icon_filename': 'icons8-reuse-80.png',
+                'long_description': f"""
+                <p>
+                If you are already using the <a href='https://www.centbee.com/'>Centbee</a>
+                mobile wallet and have your seed phrase, this option allows you to add and access
+                that same wallet in ElectrumSV.
+                </p>
+                {seed_phrase_html}
+                {original_wallet_safe_html % {"wallet_name": "Centbee"}}
+                """,
+            },
+            {
+                'page': Pages.IMPORT_WALLET_TEXT_SEED_PHRASE_HANDCASH,
+                'description': _("Import wallet using seed phrase (HandCash)"),
+                'icon_filename': 'icons8-reuse-80.png',
+                'long_description': f"""
+                <p>
+                If you are already using the <a href='https://handcash.io/'>HandCash</a>
+                mobile wallet and have your seed phrase, this option allows you to add and access
+                that same wallet in ElectrumSV.
+                </p>
+                {seed_phrase_html}
+                {original_wallet_unsafe_html % {"wallet_name": "HandCash"}}
+                """,
+            },
+            {
+                'page': Pages.IMPORT_WALLET_TEXT_SEED_PHRASE_MONEYBUTTON,
+                'description': _("Import wallet using seed phrase (MoneyButton)"),
+                'icon_filename': 'icons8-reuse-80.png',
+                'long_description': f"""
+                <p>
+                If you are already using the
+                <a href='https://www.moneybutton.com/'>Money Button</a>
+                mobile wallet and have your seed phrase, this option allows you to add and access
+                that same wallet in ElectrumSV.
+                </p>
+                {seed_phrase_html}
+                {original_wallet_unsafe_html % {"wallet_name": "Money Button"}}
+                """,
+            },
+            {
+                'page': Pages.IMPORT_HARDWARE_WALLET_FOR_DIGITALBITBOX,
+                'description': _("Import hardware wallet (Digital Bitbox)"),
+                'icon_filename': 'icons8-usb-2-80.png',
+            },
+            {
+                'page': Pages.IMPORT_HARDWARE_WALLET_FOR_KEEPKEY,
+                'description': _("Import hardware wallet (Keepkey)"),
+                'icon_filename': 'icons8-usb-2-80.png',
+            },
+            {
+                'page': Pages.IMPORT_HARDWARE_WALLET_FOR_LEDGER,
+                'description': _("Import hardware wallet (Ledger)"),
+                'icon_filename': 'icons8-usb-2-80.png',
+            },
+            {
+                'page': Pages.IMPORT_HARDWARE_WALLET_FOR_TREZOR,
+                'description': _("Import hardware wallet (Trezor)"),
+                'icon_filename': 'icons8-usb-2-80.png',
+            },
+        ]
