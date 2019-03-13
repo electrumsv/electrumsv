a8be8dd8f1208cbf4e21e6363a8757f01233df00
Neil Booth <kyuupichan@gmail.com>
2019-03-13 13:58:32 +0800

    tests: create a proxy for each module

--- electrumsv/tests/util.py
+++ electrumsv/tests/util.py
@@ -10,12 +10,15 @@ class AppStateProxyTest(AppStateProxy):
         super().__init__(config, 'qt')
         self.async_ = ASync()
 
-proxy = AppStateProxyTest()
-
+proxy = None
 
 def setup_async():
+    global proxy
+    proxy = AppStateProxyTest()
     proxy.async_.__enter__()
 
 
 def tear_down_async():
+    global proxy
     proxy.async_.__exit__(None, None, None)
+    proxy = None
