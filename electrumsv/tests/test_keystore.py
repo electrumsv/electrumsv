e07a6da9b931dd4cd5a4c76c9216a6fe098daec8
Neil Booth <kyuupichan@gmail.com>
2019-03-30 18:10:19 +0800

    keystore.py: more tests for Imported_KeyStore

--- electrumsv/tests/test_keystore.py
+++ electrumsv/tests/test_keystore.py
@@ -102,6 +102,15 @@ class TestOld_KeyStore:
         assert keystore.get_pubkey_derivation("ff") is None
 
 
+# Password b'password'; one minikey, one WIF
+keypairs_dict = {
+    '02d0de0aaeaefad02b8bdc8a01a1b8b11c696bd3d66a2c5f10780d95b7df42645c': 'tfxSNL1G2aHpNCELs8aGnmB0SvlRHgZ3tXlLmO/XoVPElTo4/Pr+Gx+icCa4Dpjd+HtfhA+5pChRumaQ5QFbzFItC0uQ+ZVtRhr/sRxakrE=',
+    '04e7dd15b4271f8308ff52ad3d3e472b652e78a2c5bc6ed10250a543d28c0128894ae863d086488e6773c4589be93a1793f685dd3f1e8a1f1b390b23470f7d1095': 'NithPh1OqOiALYK0TP4wBdZZOOWVF+wJwBakvlrH7mpho65PVR3DMuC3WAdmyMJ2'
+}
+imported_keystore = Imported_KeyStore({
+    'keypairs': keypairs_dict,
+})
+
 class TestImported_KeyStore:
 
     @pytest.mark.parametrize("WIF,pk_string", (
@@ -153,6 +162,71 @@ class TestImported_KeyStore:
         pubkey = d.import_privkey("5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ", password)
         dec_msg = d.decrypt_message(pubkey, enc_msg, password)
         assert dec_msg == b'BitcoinSV'
+
+    def test_dump(self):
+        assert imported_keystore.dump() == {'type': 'imported', 'keypairs': keypairs_dict}
+
+    def test_is_deterministic(self):
+        assert not imported_keystore.is_deterministic()
+
+    def test_can_change_password(self):
+        assert imported_keystore.can_change_password()
+
+    def test_get_master_public_key(self):
+        assert imported_keystore.get_master_public_key() is None
+
+    def test_can_import(self):
+        assert imported_keystore.can_import()
+
+    def test_get_addresses(self):
+        assert imported_keystore.get_addresses() == [
+            Address.from_string(addr) for addr in (
+                '1KXf5PUHNaV42jE9NbJFPKhGGN1fSSGJNK', '1LoVGDgRs9hTfTNJNuXKSpywcbdvwRXpmK'
+            )
+        ]
+
+    def test_address_to_pubkey(self):
+        addr = Address.from_string('1KXf5PUHNaV42jE9NbJFPKhGGN1fSSGJNK')
+        pubkey = imported_keystore.address_to_pubkey(addr)
+        assert pubkey.to_string() == (
+            '04e7dd15b4271f8308ff52ad3d3e472b652e78a2c5bc6ed10250a543d28c0128894ae'
+            '863d086488e6773c4589be93a1793f685dd3f1e8a1f1b390b23470f7d1095'
+        )
+        addr = Address.from_string('1JYPZWn7YbJJ8LpNknU1EVxs3JXuVzc7Rd')
+        assert imported_keystore.address_to_pubkey(addr) is None
+
+    def test_check_password(self):
+        imported_keystore.check_password('password')
+        with pytest.raises(InvalidPassword):
+            imported_keystore.check_password('')
+
+    def test_export_private_key(self):
+        pubkey = list(imported_keystore.keypairs.keys())[0]
+        assert imported_keystore.export_private_key(pubkey, 'password') == (
+            'KwdMAjGmerYanjeui5SHS7JkmpZvVipYvB2LJGU1ZxJwYvP98617')
+        with pytest.raises(InvalidPassword):
+            imported_keystore.export_private_key(pubkey, '')
+
+    @pytest.mark.parametrize("index,hex_str,is_compressed", (
+        (0, '0c28fca386c7a227600b2fe50b7cae11ec86d3bf1fbe471be89827e19d72aa1d', True),
+        (1, '98e315c3256a9717d4ddea30eb2a0a2d56a16493794eb0535366ea22d869a320', False),
+    ))
+    def test_get_private_key(self, index, hex_str, is_compressed):
+        pubkey = list(imported_keystore.keypairs.keys())[index]
+        assert imported_keystore.get_private_key(pubkey, 'password') == (
+            bytes.fromhex(hex_str), is_compressed)
+
+    def test_update_password(self):
+        keystore = Imported_KeyStore({'keypairs': keypairs_dict})
+        keystore.update_password('password', 'new password')
+        pubkey = list(keystore.keypairs.keys())[0]
+        assert keystore.export_private_key(pubkey, 'new password') == (
+            'KwdMAjGmerYanjeui5SHS7JkmpZvVipYvB2LJGU1ZxJwYvP98617')
+        keystore.update_password('new password', '')
+        assert keystore.export_private_key(pubkey, None) == (
+            'KwdMAjGmerYanjeui5SHS7JkmpZvVipYvB2LJGU1ZxJwYvP98617')
+        with pytest.raises(InvalidPassword):
+            keystore.export_private_key(pubkey, '')
 
 
 class TestBIP32_KeyStore:
