bb372044f07689f2f77eb9acf4621c9586b3c165
Neil Booth <kyuupichan@gmail.com>
2019-03-31 15:31:57 +0800

    address.py: remove Base58 implementation

--- electrumsv/address.py
+++ electrumsv/address.py
@@ -27,7 +27,7 @@
 from collections import namedtuple
 import struct
 
-from bitcoinx import Ops, PrivateKey
+from bitcoinx import Ops, base58_decode_check, base58_encode_check
 
 from . import cashaddr
 from .bitcoin import is_minikey, minikey_to_private_key
@@ -295,8 +295,8 @@ class Address(namedtuple("AddressTuple", "hash160 kind")):
                 raise AddressError(str(e))
 
         try:
-            raw = Base58.decode_check(string)
-        except Base58Error as e:
+            raw = base58_decode_check(string)
+        except ValueError as e:
             raise AddressError(str(e))
 
         # Require version byte(s) plus hash160.
@@ -356,7 +356,7 @@ class Address(namedtuple("AddressTuple", "hash160 kind")):
         else:
             verbyte = Net.ADDRTYPE_P2SH
 
-        return Base58.encode_check(bytes([verbyte]) + self.hash160)
+        return base58_encode_check(bytes([verbyte]) + self.hash160)
 
     def to_script(self):
         '''Return a binary script to pay to the address.'''
@@ -480,82 +480,3 @@ class Script(object):
             raise ScriptError('truncated script')
 
         return ops
-
-
-class Base58Error(Exception):
-    '''Exception used for Base58 errors.'''
-
-
-class Base58(object):
-    '''Class providing base 58 functionality.'''
-
-    chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-    assert len(chars) == 58
-    cmap = {c: n for n, c in enumerate(chars)}
-
-    @staticmethod
-    def char_value(c):
-        val = Base58.cmap.get(c)
-        if val is None:
-            raise Base58Error('invalid base 58 character "{}"'.format(c))
-        return val
-
-    @staticmethod
-    def decode(txt):
-        """Decodes txt into a big-endian bytearray."""
-        if not isinstance(txt, str):
-            raise TypeError('a string is required')
-
-        if not txt:
-            raise Base58Error('string cannot be empty')
-
-        value = 0
-        for c in txt:
-            value = value * 58 + Base58.char_value(c)
-
-        result = int_to_bytes(value)
-
-        # Prepend leading zero bytes if necessary
-        count = 0
-        for c in txt:
-            if c != '1':
-                break
-            count += 1
-        if count:
-            result = bytes(count) + result
-
-        return result
-
-    @staticmethod
-    def encode(be_bytes):
-        """Converts a big-endian bytearray into a base58 string."""
-        value = bytes_to_int(be_bytes)
-
-        txt = ''
-        while value:
-            value, mod = divmod(value, 58)
-            txt += Base58.chars[mod]
-
-        for byte in be_bytes:
-            if byte != 0:
-                break
-            txt += '1'
-
-        return txt[::-1]
-
-    @staticmethod
-    def decode_check(txt):
-        '''Decodes a Base58Check-encoded string to a payload.  The version
-        prefixes it.'''
-        be_bytes = Base58.decode(txt)
-        result, check = be_bytes[:-4], be_bytes[-4:]
-        if check != sha256d(result)[:4]:
-            raise Base58Error('invalid base 58 checksum for {}'.format(txt))
-        return result
-
-    @staticmethod
-    def encode_check(payload):
-        """Encodes a payload bytearray (which includes the version byte(s))
-        into a Base58Check string."""
-        be_bytes = payload + sha256d(payload)[:4]
-        return Base58.encode(be_bytes)
