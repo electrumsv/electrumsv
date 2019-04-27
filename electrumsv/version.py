743c0ec235f6eb59159687196541bcf31ca95981
rt121212121 <roger.taylor.email@gmail.com>
2019-04-27 16:27:00 +1200

    Prepare for the 1.2.1 release.

--- electrumsv/version.py
+++ electrumsv/version.py
@@ -1,5 +1,5 @@
-PACKAGE_VERSION = '1.2.1'                        # version of the client package
-PACKAGE_DATE = '2019-04-25T20:00:00.000000+13:00'  # official timestamp for client package
+PACKAGE_VERSION = '1.2.1'                          # version of the client package
+PACKAGE_DATE = '2019-04-28T20:00:00.000000+13:00'  # official timestamp for client package
 PROTOCOL_VERSION = '1.4'                           # protocol version requested
 
 # The hash of the mnemonic seed must begin with this
