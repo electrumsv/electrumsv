0f2d7a92bbadded25ebceea63de3e9942b2b8c24
Neil Booth <kyuupichan@gmail.com>
2019-04-28 09:20:45 +0100

    Update cashaddr tests
    Closes #105

--- electrumsv/tests/test_cashaddrenc.py
+++ electrumsv/tests/test_cashaddrenc.py
@@ -23,9 +23,10 @@
 
 """Reference tests for cashaddr adresses"""
 
-import unittest
 import random
 from electrumsv import cashaddr
+
+import pytest
 
 
 BSV_PREFIX = "bitcoincash"
@@ -53,18 +54,18 @@ VALID_HASHES = [
 ]
 
 
-class TestCashAddrAddress(unittest.TestCase):
+class TestCashAddrAddress:
     """Unit test class for cashaddr addressess."""
 
     # Valid address sizes from the cashaddr spec
     valid_sizes = [160, 192, 224, 256, 320, 384, 448, 512]
 
     def test_encode_bad_inputs(self):
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             cashaddr.encode_full(2, cashaddr.PUBKEY_TYPE, bytes(20))
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             cashaddr.encode_full(BSV_PREFIX, cashaddr.PUBKEY_TYPE, '0' * 40)
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             cashaddr.encode_full(BSV_PREFIX, 15, bytes(20))
 
     def test_encode_decode(self):
@@ -79,9 +80,9 @@ class TestCashAddrAddress(unittest.TestCase):
                 addr = cashaddr.encode_full(prefix, cashaddr.PUBKEY_TYPE,
                                             hashbytes)
                 rprefix, kind, addr_hash = cashaddr.decode(addr)
-                self.assertEqual(rprefix, prefix)
-                self.assertEqual(kind, cashaddr.PUBKEY_TYPE)
-                self.assertEqual(addr_hash, hashbytes)
+                assert rprefix == prefix
+                assert kind == cashaddr.PUBKEY_TYPE
+                assert addr_hash == hashbytes
 
     def test_bad_encode_size(self):
         """Test that bad sized hashes fail to encode."""
@@ -91,12 +92,12 @@ class TestCashAddrAddress(unittest.TestCase):
             size += 1
             # Convert to a valid number of bytes for a hash
             hashbytes = bytes(random.randint(0, 255) for i in range(size))
-            with self.assertRaises(ValueError):
+            with pytest.raises(ValueError):
                 cashaddr.encode_full(BSV_PREFIX, cashaddr.PUBKEY_TYPE,
                                      hashbytes)
 
     def test_decode_bad_inputs(self):
-        with self.assertRaises(TypeError):
+        with pytest.raises(TypeError):
             cashaddr.decode(b'foobar')
 
     def test_bad_decode_size(self):
@@ -113,7 +114,7 @@ class TestCashAddrAddress(unittest.TestCase):
             addr = BSV_PREFIX + ':' + ''.join(cashaddr._CHARSET[d] for d in payload)
             # Check decode fails.  This can trigger the length mismatch,
             # excess padding, or non-zero padding errors
-            with self.assertRaises(ValueError):
+            with pytest.raises(ValueError):
                 cashaddr.decode(addr)
 
     def test_address_case(self):
@@ -121,62 +122,55 @@ class TestCashAddrAddress(unittest.TestCase):
         assert prefix == "bitcoincash"
         prefix, _kind, _hash160 = cashaddr.decode("BITCOINCASH:PPM2QSZNHKS23Z7629MMS6S4CWEF74VCWVN0H829PQ")
         assert prefix == "BITCOINCASH"
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             cashaddr.decode("bitcoincash:PPM2QSZNHKS23Z7629MMS6S4CWEF74VCWVN0H829PQ")
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             cashaddr.decode("bitcoincash:ppm2qsznhks23z7629mmS6s4cwef74vcwvn0h829pq")
 
     def test_prefix(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             cashaddr.decode(":ppm2qsznhks23z7629mms6s4cwef74vcwvn0h82")
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             cashaddr.decode("ppm2qsznhks23z7629mms6s4cwef74vcwvn0h82")
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             cashaddr.decode("bitcoin cash:ppm2qsznhks23z7629mms6s4cwef74vcwvn0h82")
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             cashaddr.decode("bitcoin cash:ab")
         # b is invalid
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             cashaddr.decode("bitcoincash:ppm2qsznbks23z7629mms6s4cwef74vcwvn0h82")
 
-    def test_bad_decode_checksum(self):
-        """Test whether addresses with invalid checksums fail to decode."""
-        for bits_size in self.valid_sizes:
-            size = bits_size // 8
-            # Convert to a valid number of bytes for a hash
-            hashbytes = bytes(random.randint(0, 255) for i in range(size))
-            addr = cashaddr.encode_full(BSV_PREFIX, cashaddr.PUBKEY_TYPE,
-                                        hashbytes)
-            addrlist = list(addr)
-            # Inject an error
-            values = list(cashaddr._CHARSET)
-            while True:
-                pos = random.randint(0, len(addr) - 1)
-                choice = random.choice(values)
-                if choice != addrlist[pos] and addrlist[pos] in values:
-                    addrlist[pos] = choice
-                    break
 
-            mangled_addr = ''.join(addrlist)
-            with self.assertRaises(ValueError) as e:
-                cashaddr.decode(mangled_addr)
-            self.assertTrue('invalid checksum' in e.exception.args[0])
+    @pytest.mark.parametrize("mangled_addr", (
+        'qz3jljn0cms4fxkgwr2p8jgkjyw0rnxyesmq0kh2pq',
+        'q93d4uh7r2revpkch695vvssm4y6tm438lav0za49rjqwa3w',
+        'qgrkaz88sm8s8vrdq87s3uzjss6wljteguzva9vka23t5dqwsuxxhvh',
+        'qwr05vx8hu7la2tpu6fefw5ndxlc43q35mj3ny9sa4l9ylfdekwfv8r7805d6',
+        'qj9z9z2h80zj2ddhea2ywmuyw32zgjclj7rvpmt0l3s7awpnmwd4jpzc5grj7ywy4uc3g07rs4',
+        'q4zy536yrje4rdv0sus3epe2wrazvutajezma3vc9g54dkhv785kegeqklrz3emepmv3z2q39yr7pcsy660wdyy',
+        'qecf9u5myey7m8ug9us08egt7dz3rc0k892p7ywjs7a6ravy'
+        '6z874pzk7nvj7sd382ptc4q2k5e9fe39rrc6y9dp5lxqapwzajx5',
+        'qatl0fxkdj65zj6m3tvs4lrn35zca70lld279u633qxu8ex82gzmmej0ul6'
+        'xzkjtkj5ytrr2g02h576p8gv6nrlqvnky2hewm7hrqr2n2jddpgsw',
+    ))
+    def test_bad_decode_checksum(self, mangled_addr):
+        """Test whether addresses with invalid checksums fail to decode."""
+        with pytest.raises(ValueError) as e:
+            cashaddr.decode('bitcoincash:' + mangled_addr)
+        assert 'invalid checksum' in str(e.value)
 
     def test_valid_scripthash(self):
         """Test whether valid P2PK addresses decode to the correct output."""
         for (address, hashbytes) in zip(VALID_SCRIPT_ADDRESSES, VALID_HASHES):
             rprefix, kind, addr_hash = cashaddr.decode(address)
-            self.assertEqual(rprefix, BSV_PREFIX)
-            self.assertEqual(kind, cashaddr.SCRIPT_TYPE)
-            self.assertEqual(addr_hash, hashbytes)
+            assert rprefix == BSV_PREFIX
+            assert kind == cashaddr.SCRIPT_TYPE
+            assert addr_hash == hashbytes
 
     def test_valid_pubkeys(self):
         """Test whether valid P2SH addresses decode to the correct output."""
         for (address, hashbytes) in zip(VALID_PUBKEY_ADDRESSES, VALID_HASHES):
             rprefix, kind, addr_hash = cashaddr.decode(address)
-            self.assertEqual(rprefix, BSV_PREFIX)
-            self.assertEqual(kind, cashaddr.PUBKEY_TYPE)
-            self.assertEqual(addr_hash, hashbytes)
-
-if __name__ == '__main__':
-    unittest.main()
+            assert rprefix == BSV_PREFIX
+            assert kind == cashaddr.PUBKEY_TYPE
+            assert addr_hash == hashbytes
