5059e1e0a8ecfcfb68415e4bb8869cf1a0732850
rt121212121 <roger.taylor.email@gmail.com>
2019-05-12 12:57:48 +1200

    Make payment dialog. Fix up account selection.

--- electrumsv/gui/qt/payment.py
+++ electrumsv/gui/qt/payment.py
@@ -1,16 +1,22 @@
 from typing import Optional
 
 from PyQt5.QtCore import (QAbstractItemModel, Qt, QSize, QSortFilterProxyModel, QVariant,
-    QModelIndex, QEvent)
-from PyQt5.QtGui import QPainter, QPixmap, QPalette
+    QModelIndex, pyqtSignal, QEvent, QObject)
+from PyQt5.QtGui import QPainter, QPixmap, QPalette, QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import (QAction, QComboBox, QCompleter, QDialog, QDialogButtonBox,
     QFrame, QGridLayout, QHBoxLayout, QLabel, QLineEdit, QMenu, QPushButton,
     QSizePolicy, QStyledItemDelegate, QTabWidget, QVBoxLayout, QWidget, QLayout,
-    QStyleOptionViewItem, QStyle, QStyleOption)
+    QStyleOptionViewItem, QStyle, QStyleOption, QTableView, QAbstractItemView)
 
+from electrumsv.app_state import app_state
 from electrumsv.i18n import _
 
 from .util import icon_path, read_QIcon
+
+
+# TODO: Handle case where there are no contacts.
+#       - ...
+# TODO: XXX
 
 
 payee_badge_css = """
@@ -21,6 +27,8 @@ payee_badge_css = """
         border-style: solid;
         padding-left: 4px;
         padding-right: 4px;
+        padding-top: 2px;
+        padding-bottom: 2px;
     }
 
     #PayeeBadgeName {
@@ -57,70 +65,173 @@ payee_badge_css = """
 
 
 class PaymentAmountWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, local_api, parent=None):
         super().__init__(parent)
+        self._local_api = local_api
 
         self.setObjectName("PaymentAmount")
 
         amount_widget = QLineEdit()
         currency_widget = QLineEdit()
 
-        combo = QComboBox()
-        combo.setEditable(True)
+        currency_combo = QComboBox()
+        currency_combo.setEditable(True)
 
-        filter_model = QSortFilterProxyModel(combo)
+        filter_model = QSortFilterProxyModel(currency_combo)
         filter_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
-        filter_model.setSourceModel(combo.model())
+        filter_model.setSourceModel(currency_combo.model())
 
-        contact_completer = QCompleter(filter_model, combo)
+        contact_completer = QCompleter(filter_model, currency_combo)
         contact_completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
-        combo.setCompleter(contact_completer)
+        currency_combo.setCompleter(contact_completer)
 
-        options = [
-            "BSV",
-            "USD",
-        ]
+        # base unit.
+        # selected fiat currency.
 
-        combo.addItems(options)
-        combo.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Fixed)
+        options = []
+        base_unit = local_api.get_base_unit()
+        options.append(base_unit)
+        fiat_unit = local_api.get_fiat_unit()
+        if fiat_unit is not None:
+            options.append(fiat_unit)
+
+        currency_combo.addItems(options)
+        currency_combo.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Fixed)
 
         layout = QHBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(currency_combo)
         layout.addWidget(amount_widget)
-        layout.addWidget(combo)
         self.setLayout(layout)
+
+        self._currency_combo_options = options
+        self._currency_combo = currency_combo
+        self._amount_widget = amount_widget
+        self._local_api.set_payment_amount.connect(self._set_payment_amount)
+
+    def _set_payment_amount(self, balance_widget) -> None:
+        currency = balance_widget.balance_currency
+        amount = balance_widget.balance_amount
+
+        idx = self._currency_combo_options.index(currency)
+        self._currency_combo.setCurrentIndex(idx)
+        self._amount_widget.setText(amount)
 
 
 class FundsSelectionWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, local_api, parent=None):
         super().__init__(parent)
+        self._local_api = local_api
 
         self.setObjectName("FundsSelector")
 
+        balance = local_api.get_balance()
+        sv_text, fiat_text = local_api.wallet_window.get_amount_and_units(balance)
+
+        if fiat_text:
+            column_count = 3
+        else:
+            column_count = 2
+        model = QStandardItemModel(1, 3, self)
+        model.setItem(0, 0, QStandardItem(_("All available funds")))
+        sv_item = QStandardItem(sv_text)
+        sv_item.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        model.setItem(0, 1, sv_item)
+        if fiat_text:
+            fiat_item = QStandardItem(fiat_text)
+            fiat_item.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            model.setItem(0, 2, fiat_item)
+
+        tableView = QTableView(self)
+        tableView.setObjectName("FundsSelectionPopup")
+        tableView.setWordWrap(False)
+        tableView.setModel(model)
+        tableView.verticalHeader().setVisible(False)
+        tableView.horizontalHeader().setVisible(False)
+        tableView.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        tableView.setSelectionBehavior(QAbstractItemView.SelectRows)
+        tableView.setAutoScroll(False)
+        tableView.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Fixed)
+        # Show more compact rows, this will actually be larger forced out by the contents to be
+        # just the necessary size.
+        tableView.setRowHeight(0, 20)
+
         combo = QComboBox()
-        combo.setEditable(True)
+        combo.setModel(model)
+        combo.setView(tableView)
+        combo.setMinimumWidth(300)
 
-        filter_model = QSortFilterProxyModel(combo)
-        filter_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
-        filter_model.setSourceModel(combo.model())
+        # Detect when the combobox popup view is shown by rebinding and wrapping the method.
+        def _new_showPopup(_self: QTableView) -> None:
+            nonlocal old_showPopup, tableView
+            old_showPopup()
+            tableView.resizeColumnsToContents()
 
-        contact_completer = QCompleter(filter_model, combo)
-        contact_completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
-        combo.setCompleter(contact_completer)
+        old_showPopup = combo.showPopup
+        setattr(combo, "showPopup", _new_showPopup.__get__(combo, combo.__class__))
 
-        # TODO: Get total available funds and append to the list entry.
+        hlayout1 = QHBoxLayout()
+        hlayout1.setSpacing(0)
+        hlayout1.setContentsMargins(0, 0, 0, 2)
+        hlayout1.addWidget(combo, 1)
 
-        options = [
-            "All available funds",
-        ]
+        hlayout2 = QHBoxLayout()
+        hlayout2.setSpacing(0)
+        hlayout2.setContentsMargins(0, 2, 0, 0)
+        balance_icon_label = QLabel("")
+        balance_icon_label.setPixmap(QPixmap(icon_path("sb_balance.png")))
+        balance_icon_label.setToolTip(_("The balance of the selected account."))
+        hlayout2.addWidget(balance_icon_label)
+        hlayout2.addSpacing(4)
+        sv_balance = QLineEdit(sv_text)
+        sv_balance.balance_currency = local_api.get_base_unit()
+        sv_balance.balance_amount = local_api.get_base_amount(balance)
+        sv_balance.setAlignment(Qt.AlignHCenter)
+        sv_balance.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sv_balance.setReadOnly(True)
+        hlayout2.addWidget(sv_balance)
+        if fiat_text:
+            hlayout2.addSpacing(4)
+            fiat_balance = QLineEdit(fiat_text)
+            fiat_balance.balance_currency = local_api.get_fiat_unit()
+            fiat_balance.balance_amount = local_api.get_fiat_amount(balance)
+            fiat_balance.setAlignment(Qt.AlignHCenter)
+            fiat_balance.setReadOnly(True)
+            hlayout2.addWidget(fiat_balance)
 
-        combo.addItems(options)
+        vlayout = QVBoxLayout()
+        vlayout.setSpacing(0)
+        vlayout.setContentsMargins(0, 0, 0, 0)
+        vlayout.addLayout(hlayout1)
+        vlayout.addLayout(hlayout2)
+        self.setLayout(vlayout)
 
-        self.combo = combo
+        self._sv_balance = sv_balance
+        self._fiat_balance = fiat_balance
 
-        layout = QHBoxLayout()
-        # layout.addWidget(combo)
-        self.setLayout(layout)
+        sv_balance.installEventFilter(self)
+        fiat_balance.installEventFilter(self)
+
+    def paintEvent(self, event):
+        opt = QStyleOption()
+        opt.initFrom(self)
+        p = QPainter(self)
+        self.style().drawPrimitive(QStyle.PE_Widget, opt, p, self)
+
+    def eventFilter(self, obj, evt):
+        # Clicking a balance field sets the amount currency and the amount.
+        if obj is self._sv_balance or obj is self._fiat_balance:
+            if self._checkLineEditEvent(evt):
+                self._local_api.set_payment_amount.emit(obj)
+        return False
+
+    def _checkLineEditEvent(self, evt: QEvent) -> bool:
+        if evt.type() == QEvent.MouseButtonPress:
+            return True
+        if evt.type() == QEvent.KeyPress:
+            if evt.key() == Qt.Key_Return or evt.key() == Qt.Key_Space or evt.key() == Qt.Key_Enter:
+                return True
+        return False
 
 
 class PayeeBadge(QWidget):
@@ -274,6 +385,7 @@ class PayeeSearchWidget(QWidget):
 
         edit_field = QLineEdit()
         edit_field.setMinimumWidth(200)
+        edit_field.setPlaceholderText("Type a contact name here..")
 
         filter_model = QSortFilterProxyModel(edit_field)
         filter_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
@@ -369,7 +481,7 @@ class PaymentSectionWidget(QWidget):
         label = QLabel(title_text +":")
         label.setObjectName("PaymentSectionTitle")
         label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        self.frame_layout.addWidget(label)
+        self.frame_layout.addWidget(label, Qt.AlignTop)
 
     def add_row(self, label_text: QWidget, field_widget: QWidget,
             stretch_field: bool=False) -> None:
@@ -392,10 +504,12 @@ class PaymentSectionWidget(QWidget):
         label_layout = QHBoxLayout()
         label_layout.addWidget(label)
         label_layout.addWidget(help_label)
+        # Ensure that the top-aligned vertical spacing matches the fields.
+        label_layout.setContentsMargins(0,2,0,2)
         label_layout.setSizeConstraint(QLayout.SetFixedSize)
 
         grid_layout = QGridLayout()
-        grid_layout.addLayout(label_layout, 0, 0, Qt.AlignLeft)
+        grid_layout.addLayout(label_layout, 0, 0, Qt.AlignLeft | Qt.AlignTop)
         if stretch_field:
             grid_layout.addWidget(field_widget, 0, 1)
         else:
@@ -426,11 +540,12 @@ class PaymentPayeeWidget(PaymentSectionWidget):
 
 
 class PaymentFundingWidget(PaymentSectionWidget):
-    def __init__(self, parent=None) -> None:
+    def __init__(self, local_api, parent=None) -> None:
         super().__init__(parent)
+        self._local_api = local_api
 
-        from_widget = FundsSelectionWidget().combo
-        amount_widget = PaymentAmountWidget()
+        from_widget = FundsSelectionWidget(local_api)
+        amount_widget = PaymentAmountWidget(local_api)
 
         self.add_title(_("Payment details"))
         self.add_row(_("Pay from"), from_widget)
@@ -443,6 +558,7 @@ class PaymentNoteWidget(PaymentSectionWidget):
 
         yours_widget = QLineEdit()
         theirs_widget = QLineEdit()
+        theirs_widget.setEnabled(False)
 
         self.add_title(_("Payment notes"))
         self.add_row(_("Yours"), yours_widget, stretch_field=True)
@@ -455,23 +571,47 @@ class PaymentDetailsFormWidget(QWidget):
 
         payee_widget = PaymentPayeeWidget(local_api)
 
-        funding_widget = PaymentFundingWidget()
+        funding_widget = PaymentFundingWidget(local_api)
 
         note_widget = PaymentNoteWidget()
         self.notes = note_widget
+
+        def _on_next_tab(checked=False):
+            current_index = local_api.payment_window.tabs.currentIndex()
+            local_api.payment_window.tabs.setCurrentIndex(current_index+1)
+
+        confirm_button = QPushButton(_("Next >>"))
+        confirm_button.setAutoDefault(False)
+        confirm_button.clicked.connect(_on_next_tab)
+        confirm_button.setEnabled(False)
+
+        confirm_layout = QHBoxLayout()
+        confirm_layout.setSpacing(0)
+        confirm_layout.setContentsMargins(0, 0, 0, 0)
+        confirm_layout.addStretch(1)
+        confirm_layout.addWidget(confirm_button)
 
         vlayout = QVBoxLayout()
         vlayout.addWidget(payee_widget)
         vlayout.addWidget(funding_widget)
         vlayout.addWidget(note_widget)
+        vlayout.addLayout(confirm_layout)
+        self.setLayout(vlayout)
+
+
+class ConfirmPaymentFormWidget(QWidget):
+    def __init__(self, local_api, parent=None) -> None:
+        super().__init__(parent)
+
+        vlayout = QVBoxLayout()
         self.setLayout(vlayout)
 
 
 class PaymentWindow(QDialog):
     def __init__(self, parent=None):
-        _local_api = _LocalApi(parent, self)
-
         super().__init__(parent)
+
+        _local_api = _LocalApi(parent, self)
 
         self.setWindowTitle("Payment")
 
@@ -501,23 +641,35 @@ class PaymentWindow(QDialog):
         #PaymentSectionLabel {
             color: grey;
         }
+
+        QLineEdit:read-only {
+            background-color: #F2F2F2;
+        }
+
+        QTableView#FundsSelectionPopup::item {
+            padding-left: 4px;
+            padding-right: 4px;
+        }
         """ + payee_badge_css)
 
         # green for badge: 4AC41C
         # red for badge: D8634C
 
         enter_details_widget = PaymentDetailsFormWidget(_local_api)
-        enter_details_widget.setContentsMargins(0, 0, 0, 0)
+        confirm_details_widget = ConfirmPaymentFormWidget(_local_api)
 
-        corner_text = _("Make a payment")
-        corner_widget = QLabel(corner_text)
-        corner_widget.setObjectName("cornerWidget")
+        # Does not look good on MacOS, due to shifted alignment.
+        # corner_text = _("Make a payment")
+        # corner_widget = QLabel(corner_text)
+        # corner_widget.setObjectName("cornerWidget")
 
         self.form = enter_details_widget
 
         self.tabs = tabs = QTabWidget()
-        tabs.addTab(enter_details_widget, "Details")
-        tabs.setCornerWidget(corner_widget)
+        # tabs.setCornerWidget(corner_widget)
+        details_idx = tabs.addTab(enter_details_widget, _("Details"))
+        confirm_idx = tabs.addTab(confirm_details_widget, _("Confirm"))
+        tabs.setTabEnabled(confirm_idx, False)
 
         bbox = QDialogButtonBox(QDialogButtonBox.Close)
         bbox.rejected.connect(self.reject)
@@ -531,18 +683,35 @@ class PaymentWindow(QDialog):
         vlayout.addWidget(bbox)
         self.setLayout(vlayout)
 
-        self.installEventFilter(enter_details_widget)
 
-    def eventFilter(self, ob, event):
-        if event.type() == QEvent.FocusIn:
-            print(f"Fout {ob}")
+class _LocalApi(QObject):
+    set_payment_amount = pyqtSignal(object)
 
-
-class _LocalApi(object):
     def __init__(self, wallet_window, payment_window) -> None:
         self.wallet_window = wallet_window
         self.payment_window = payment_window
+        super().__init__(payment_window)
 
     def get_contacts(self):
         return list(self.wallet_window.contacts.get_contacts())
+
+    def get_balance(self, account_id=None) -> int:
+        c, u, x = self.wallet_window.wallet.get_balance()
+        return c + u
+
+    def get_fiat_unit(self) -> Optional[str]:
+        fx = app_state.fx
+        if fx and fx.is_enabled():
+            return fx.get_currency()
+
+    def get_fiat_amount(self, sv_value: int) -> Optional[str]:
+        fx = app_state.fx
+        if fx and fx.is_enabled():
+            return fx.format_amount(sv_value)
+
+    def get_base_unit(self) -> str:
+        return app_state.base_unit()
+
+    def get_base_amount(self, sv_value: int) -> str:
+        return self.wallet_window.format_amount(sv_value)
 
