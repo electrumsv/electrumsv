0d45e735230a3492f8d8d1f6ec1586937529cc12
rt121212121 <roger.taylor.email@gmail.com>
2019-12-04 20:01:02 +1300

    Close #241, wallet opened online, but had no main server while UI was opening, would error trying to display the banner of the main server it didn't have yet.

--- electrumsv/gui/qt/main_window.py
+++ electrumsv/gui/qt/main_window.py
@@ -232,7 +232,8 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin):
             # partials, lambdas or methods of subobjects.  Hence...
             self.network.register_callback(self.on_network, interests)
             # set initial message
-            self.console.showMessage(self.network.main_server.state.banner)
+            if self.network.main_server:
+                self.console.showMessage(self.network.main_server.state.banner)
             self.network.register_callback(self.on_quotes, ['on_quotes'])
             self.network.register_callback(self._on_history, ['on_history'])
             self.network.register_callback(self._on_addresses_updated, ['on_addresses_updated'])
