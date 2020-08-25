52663cd36da44540f3b54580c3b63431423ec99b
AustEcon <AustEcon0922@gmail.com>
2020-08-25 16:13:54 +1200

    generate new regtest blocks until there is sufficient balance for a topup

--- electrumsv/regtest_support.py
+++ electrumsv/regtest_support.py
@@ -1,11 +1,13 @@
 import json
 import logging
 import os
-from typing import List
+from typing import List, Optional
 
 import requests
 from bitcoinx import Headers, MissingHeader, CheckPoint, bits_to_work, P2PKH_Address, \
     hash_to_hex_str
+from electrumsv_node import electrumsv_node
+
 from electrumsv.bitcoin import COINBASE_MATURITY
 
 from electrumsv.networks import Net, BLOCK_HEIGHT_OUT_OF_RANGE_ERROR
@@ -46,24 +48,27 @@ def setup_regtest(app_state) -> HeadersRegTestMod:
         Net.MIN_CHECKPOINT_HEIGHT)
     logger.info("using regtest network - miner funds go to: '%s' (not part of this wallet)",
                 Net.REGTEST_P2PKH_ADDRESS)
-    logger.info("top-up via 'regtest_topup_account' method")
     return HeadersRegTestMod.from_file(Net.COIN, app_state.headers_filename(), Net.CHECKPOINT)
 
 
-def regtest_topup_account(receive_address: P2PKH_Address, max_amount: int=25) -> str:
+def regtest_topup_account(receive_address: P2PKH_Address, amount: int=25) -> Optional[str]:
     matured_balance = regtest_get_mined_balance()
-    # Sweep up to 25 coins to wallet receive address
-    amount = min(max_amount, matured_balance)
-    if not amount > 0:
-        logger.error("Insufficient funds in regtest slush fund to topup wallet. Mine "
-            "more blocks")
+    while matured_balance < amount:
+        nblocks = 1
+        if matured_balance == 0:
+            nblocks = 200
+        result = electrumsv_node.call_any("generatetoaddress", nblocks, Net.REGTEST_P2PKH_ADDRESS)
+        if result.status_code == 200:
+            logger.debug(f"generated {nblocks}: {result.json()['result']}")
+        matured_balance = regtest_get_mined_balance()
 
     # Note: for bare multi-sig support may need to craft rawtxs manually via bitcoind's
     #  'signrawtransaction' jsonrpc method - AustEcon
     payload = json.dumps({"jsonrpc": "2.0", "method": "sendtoaddress",
                           "params": [receive_address.to_string(), amount], "id": 0})
     result = requests.post("http://rpcuser:rpcpassword@127.0.0.1:18332", data=payload)
-    result.raise_for_status()
+    if result.status_code != 200:
+        raise requests.exceptions.HTTPError(result.text)
     txid = result.json()['result']
     logger.info("topped up wallet with %s coins to receive address='%s'. txid=%s", amount,
         receive_address.to_string(), txid)
