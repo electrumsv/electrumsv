976a631ae12a79eca63f0e9caed0f0f569852785
rt121212121 <roger.taylor.email@gmail.com>
2020-11-16 19:05:36 +1300

    Build hashes and signatures.

--- docs/website/pelicanconf.py
+++ docs/website/pelicanconf.py
@@ -49,11 +49,11 @@ class DownloadEntry(NamedTuple):
     files: List[DownloadFileEntry]
 
 DOWNLOAD_LATEST = DownloadEntry("1.3.9", "2020/11/16", "https://medium.com/@roger-taylor/electrumsv-1-3-9-50c39bc7ef68", [
-    DownloadFileEntry("fab fa-apple", "MacOS downloads", "MacOS", "ElectrumSV-1.3.9.dmg","31.5 MB"),
-    DownloadFileEntry("fab fa-windows", "Windows downloads", "Windows", "ElectrumSV-1.3.9.exe", "26.9 MB"),
-    DownloadFileEntry("fab fa-windows", "Windows downloads", "Windows", "ElectrumSV-1.3.9-portable.exe", "26.9 MB"),
+    DownloadFileEntry("fab fa-apple", "MacOS downloads", "MacOS", "ElectrumSV-1.3.9.dmg","30.3 MB"),
+    DownloadFileEntry("fab fa-windows", "Windows downloads", "Windows", "ElectrumSV-1.3.9.exe", "26.4 MB"),
+    DownloadFileEntry("fab fa-windows", "Windows downloads", "Windows", "ElectrumSV-1.3.9-portable.exe", "26.4 MB"),
     DownloadFileEntry("fas fa-code", "Other downloads", "Source code", "ElectrumSV-1.3.9.tar.gz", "7.3 MB"),
-    DownloadFileEntry("fas fa-code", "Other downloads", "Source code", "ElectrumSV-1.3.9.zip", "7.5 MB"),
+    DownloadFileEntry("fas fa-code", "Other downloads", "Source code", "ElectrumSV-1.3.9.zip", "7.6 MB"),
     DownloadFileEntry("fas fa-book", "Documentation", "HTML", "ElectrumSV-1.3.9-docs.zip", "11.5 MB"),
 ])
 
