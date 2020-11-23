f712b8666c22aa0a0730d70ae6a64a41ff91f6bf
rt121212121 <roger.taylor.email@gmail.com>
2020-11-24 11:52:33 +1300

    Untested fix for #570, where Ledger devices are not being recognised.
    Taken from: https://github.com/Electron-Cash/Electron-Cash/pull/2061

--- electrumsv/devices/ledger/ledger.py
+++ electrumsv/devices/ledger/ledger.py
@@ -460,14 +460,14 @@ class LedgerPlugin(HW_PluginBase):
                    (0x2581, 0x3b7c), # HW.1 ledger production
                    (0x2581, 0x4b7c), # HW.1 ledger test
                    (0x2c97, 0x0000), # Blue
+                   (0x2c97, 0x0011), # Blue app-bitcoin >= 1.5.1
+                   (0x2c97, 0x0015), # Blue app-bitcoin >= 1.5.1
                    (0x2c97, 0x0001), # Nano-S
+                   (0x2c97, 0x1011), # Nano-S app-bitcoin >= 1.5.1
+                   (0x2c97, 0x1015), # Nano-S app-bitcoin >= 1.5.1
                    (0x2c97, 0x0004), # Nano-X
-                   (0x2c97, 0x0005), # RFU
-                   (0x2c97, 0x0006), # RFU
-                   (0x2c97, 0x0007), # RFU
-                   (0x2c97, 0x0008), # RFU
-                   (0x2c97, 0x0009), # RFU
-                   (0x2c97, 0x000a)  # RFU
+                   (0x2c97, 0x4011), # Nano-X app-bitcoin >= 1.5.1
+                   (0x2c97, 0x4015), # Nano-X app-bitcoin >= 1.5.1
                  ]
 
     def __init__(self, name):
