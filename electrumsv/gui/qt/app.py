070fa561ed42215826c34474f45c16a0c793c663
rt121212121 <roger.taylor.email@gmail.com>
2020-11-30 09:48:49 +1300

    Release preparation for 1.3.11

--- electrumsv/gui/qt/app.py
+++ electrumsv/gui/qt/app.py
@@ -355,7 +355,7 @@ class SVApplication(QApplication):
 
     def initial_dialogs(self) -> None:
         '''Suppressible dialogs that are shown when first opening the app.'''
-        dialogs.show_named('welcome-ESV-1.3.10')
+        dialogs.show_named('welcome-ESV-1.3.11')
 
     def event_loop_started(self) -> None:
         self.cosigner_pool = CosignerPool()
--- electrumsv/gui/qt/dialogs.py
+++ electrumsv/gui/qt/dialogs.py
@@ -133,7 +133,7 @@ def show_named(name, *, parent=None, wallet=None, **kwargs):
         raise ValueError(f'no box with name {name} found')
     return box.result(parent, wallet, **kwargs)
 
-article_uri = "https://medium.com/@roger-taylor/electrumsv-1-3-10-f0a60d031d81"
+article_uri = "https://medium.com/@roger-taylor/electrumsv-1-3-11-6f09f2aaed94"
 
 take_care_notice = """
 <span>ElectrumSV is just a Bitcoin SV wallet, it cannot stop you from making bad decisions. Read
@@ -144,8 +144,8 @@ Your coins are your responsibility, take care with them.</span>
 """
 
 all_boxes = [
-    InfoBox('welcome-ESV-1.3.10',
-            _('Welcome to ElectrumSV 1.3.10'),
+    InfoBox('welcome-ESV-1.3.11',
+            _('Welcome to ElectrumSV 1.3.11'),
             '<p>'+ take_care_notice +'</p>'+
             '<p>You can read about the changes included in this release, in the '+
             f'<a href="{article_uri}">release guide</a> we have written.</p>'
