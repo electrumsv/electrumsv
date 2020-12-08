0550b7e3be9fca69d9b696ba4c82448d1d4e1814
rt121212121 <roger.taylor.email@gmail.com>
2020-12-09 12:31:49 +1300

    This is a work in progress. It does not currently work.
    * Transaction deltas table has been removed. Views are provided for now, to same effect.
    * Table-based database support has started being migrated away to function-based.
    * Indexer driven key usage is disabled and will be replaced.
    * Key deactivation feature is disabled and will be replaced.
    * Database functions that are used in migration 22 have been separated from the latest versions for backwards-compatible migration support.
    * Refactoring the "transaction metadata" object away.
    * Probably lots more.

--- electrumsv/constants.py
+++ electrumsv/constants.py
@@ -63,6 +63,8 @@ class TxFlags(IntFlag):
     # The transaction spends conflict with other transactions and it is not linked to any account.
     CONFLICTING = 1 << 7
 
+    Conflicting = 1 << 7
+
     # Complete transactions must always be added with bytedata. We no longer use this flag.
     # There will be incomplete transactions which may allow b'' perhaps, and which should be
     # updateable, but we're not there yet.
