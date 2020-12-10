9aad5723f841aac017122849575732591394f95d
AustEcon <AustEcon0922@gmail.com>
2020-12-11 03:49:28 +1300

    PR #590 - Add transaction state websocket and update functional tests
    - Client registers arrays of txids (may be only 1 tx)
    - Server responds with current tx state and then push notifications of any subsequent state changes
    - Add an example websocket client
    - Websocket connection should auto-load wallet and fixed bug where outstanding websocket connections were not being closed correctly with SIGINT
    - Update documentation
    - Switch client message to json object for consistency across the full restapi
    - Improve error handling on server and example client
    - Use 'transaction_added' event
    - Reinstate disabled functional tests (utilising new websocket functionality)
    - Added functional tests for websocket waiting for mempool and confirmation events
    - Ipdate linux shell script for running functional tests
    - Update .bat script for windows functional tests
    - Install sdk components before start (due to recent sdk changes)
    - Change tx_registrations_map to uuid: set of tx_hashes to simplify cleanup by deleting the key: value pair.
    - Delete the websocket from the tx_registrations_map and ws_clients map after disconnect
    - Addressing review changes
    - Publish node, electrumx and electrumsv logs as an artifact for inspection

--- contrib/run_func_tests.sh
+++ contrib/run_func_tests.sh
@@ -10,12 +10,16 @@ python3 -m pip install pytest pytest-cov
 # "ensuring all components are stopped..."
 electrumsv-sdk stop
 
+# "starting up node, electrumx and electrumsv in preparation for functional testing..."
+electrumsv-sdk install --background node
+electrumsv-sdk install --background electrumx
+electrumsv-sdk install --background --repo=$ESVDIR electrumsv
+
 # "resetting node, electrumx and electrumsv..."
 electrumsv-sdk reset node
 electrumsv-sdk reset electrumx
-electrumsv-sdk reset --repo=$ESVDIR
+electrumsv-sdk reset --repo=$ESVDIR electrumsv
 
-# "starting up node, electrumx and electrumsv in preparation for functional testing..."
 electrumsv-sdk start --background node
 electrumsv-sdk start --background electrumx
 electrumsv-sdk start --background --repo=$ESVDIR electrumsv
--- examples/applications/restapi/app.py
+++ examples/applications/restapi/app.py
@@ -1,10 +1,12 @@
 import concurrent
+import json
 import logging
 import time
-from typing import List, Optional, Callable
+from typing import Any, Callable, Iterable, List, Optional
 
 import bitcoinx
 from aiorpcx import run_in_thread
+from bitcoinx import hash_to_hex_str
 
 from electrumsv.app_state import app_state
 from electrumsv.transaction import Transaction
@@ -39,11 +41,58 @@ class RESTAPIApplication:
         # app_state.daemon is initialised after app. Setup things dependent on daemon here.
         self.logger.debug("setting up daemon-app")
         self.restapi = ExtensionEndpoints()
+
+        # state management of the websocket must be via the aiohttp.web.Application object
+        # access from TxStateWebSocket View via self.request.app['ws_clients'] and
+        self.aiohttp_web_app = self.app_state.daemon.rest_server.app
+        self.aiohttp_web_app['ws_clients'] = {}  # uuid: WSClient
+        self.aiohttp_web_app['tx_registrations_map'] = {}  # uuid: set of tx_hashes
+        self.aiohttp_web_app['restapi'] = self.restapi
         self.app_state.daemon.rest_server.register_routes(self.restapi)
 
     def _teardown_app(self) -> None:
-        pass
+        for client in self.aiohttp_web_app['ws_clients'].values():
+            app_state.async_.spawn(client.websocket.close())
 
     def on_new_wallet_event(self, wallet_path, row) -> None:
         # an expected api when resetting / creating a new wallet...
         pass
+
+    def on_triggered_event(self, *event_data: Iterable[Any]):
+        self.app_state.async_.spawn(self.async_on_triggered_event(*event_data))
+
+    async def async_on_triggered_event(self, *event_data: Any) -> None:
+        event_name = event_data[0]
+        if event_name == WalletEventNames.TRANSACTION_STATE_CHANGE:
+            _event_name, _acc_id, tx_hash, existing_flags, updated_flags = event_data
+            await self._tx_state_push_notification(tx_hash)
+        elif event_name == WalletEventNames.TRANSACTION_ADDED:
+            _event_name, tx_hash, _tx, _involved_account_ids, _external = event_data
+            await self._tx_state_push_notification(tx_hash)
+        elif event_name == WalletEventNames.VERIFIED:
+            _event_name, tx_hash, height, conf, timestamp = event_data
+            await self._tx_state_push_notification(tx_hash)
+
+    async def _tx_state_push_notification(self, tx_hash):
+        """send push notification to all relevant websockets for the tx_hash"""
+        websocket_ids = self.aiohttp_web_app['tx_registrations_map'].keys()
+        for ws_id in websocket_ids:
+            # only notify relevant websockets
+            if tx_hash in self.aiohttp_web_app['tx_registrations_map'][ws_id]:
+                client = self.aiohttp_web_app['ws_clients'][ws_id]
+                tx_flags = client.account._wallet._transaction_cache.get_flags(tx_hash)
+                if not tx_flags:
+                    response_json = json.dumps({
+                        "code": Errors.GENERIC_BAD_REQUEST_CODE,
+                        "message": f"this txid: {hash_to_hex_str(tx_hash)} does not belong to "
+                                   f"this account_id: {client.account._id}"
+                    })
+                    self.logger.debug(f"push notification to websocket={ws_id}: {response_json}")
+                    await client.websocket.send_str(response_json)
+                    continue
+                response_json = json.dumps({
+                    "txid": hash_to_hex_str(tx_hash),
+                    "tx_flags": int(tx_flags)
+                })
+                self.logger.debug(f"push notification to websocket {ws_id}: {response_json}")
+                await client.websocket.send_str(response_json)
