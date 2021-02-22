1642fbb9759213707e82df5469e508bc06839a64
AustEcon <AustEcon0922@gmail.com>
2021-02-22 20:26:25 +1300

    Update requirements incl. Pyt5.15.2 & use pip-tools/pip compile
    for generating deterministic hashes (instead of hashin which
    seems to be less well maintained & much less popular and is
    causing issues for me on Windows 10)
    
    PyQt5.15.2 is needed for python3.9 support

--- contrib/freeze_packages.py
+++ contrib/freeze_packages.py
@@ -4,65 +4,28 @@ import shutil
 import subprocess
 import sys
 
-venv_dir = os.path.join(os.path.expanduser('~'), ".electrum-sv-venv")
 CONTRIB_PATH = os.path.dirname(os.path.realpath(__file__))
-
-if shutil.which("virtualenv") is None:
-    print("Please install virtualenv", file=sys.stderr)
-    sys.exit(1)
-
-if sys.platform == "win32":
-    venv_script_dirname = "Scripts"
-else:
-    venv_script_dirname = "bin"
 
 python_exe = shutil.which("python3")
 if sys.platform == "win32":
     python_exe = sys.executable
 
+print(f"Installing pip-tools")
+subprocess.run([ python_exe, "-m", "pip", "install", "pip-tools" ])
+
 for r_suffix in [ '-pyinstaller', '',  '-hw', '-binaries' ]:
-    if os.path.exists(venv_dir):
-        shutil.rmtree(venv_dir)
-
-    print(f"virtualenv -p \"{sys.executable}\" {venv_dir}")
-    os.system(f"virtualenv -p \"{sys.executable}\" {venv_dir}")
-
-    venv_python = os.path.join(venv_dir, venv_script_dirname, "python")
-    venv_pip = os.path.join(venv_dir, venv_script_dirname, "pip")
-
     requirements_filename = f"requirements{r_suffix}.txt"
-    print(f"Installing dependencies for '{requirements_filename}'")
-
     r_path = os.path.join(CONTRIB_PATH, "requirements", requirements_filename)
-    result = subprocess.run([ venv_pip,
-        "--disable-pip-version-check",
-        "install", "-r", r_path, "--upgrade" ])
-
-    result = subprocess.run([ venv_pip, 'freeze', '--all'], stdout=subprocess.PIPE)
-    package_names = result.stdout.splitlines()
-
-    print("OK.")
-
-    print("Generating package hashes...")
-    subprocess.run([ venv_pip,
-        "--disable-pip-version-check",
-        "install", "hashin" ])
-
     dr_path = os.path.join(CONTRIB_PATH, "deterministic-build", f"requirements{r_suffix}.txt")
-    # If these files are not updating correctly, they can be forced to update by deleting them.
-    # But hashin will not write to a non-existent requrirements file, so we should recreate them.
-    if not os.path.exists(dr_path):
-        with open(dr_path, 'w') as f:
-            f.write('')
 
-    print(f".. target: {dr_path}")
-    for package_name in package_names:
-        print(f".. hashing {package_name}")
-        subprocess.run(
-            [ venv_python, "-m", "hashin", "-r", dr_path, bytes.decode(package_name, 'utf-8') ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
-
+    """  Quote from documentation of pip-tools:
+    In future versions, the ``--allow-unsafe`` behavior will be used by default
+    and the option will be removed. It is recommended to pass the argument now to
+    adapt projects to the upcoming change.
+    """
+    # The only affected dependency for ElectrumSV is setuptools which we pin to 51.0.0 currently
+    subprocess.check_call(f"pip-compile --allow-unsafe --generate-hashes --output-file={dr_path}"
+                          f" {r_path}")
     print("OK.")
 
 print("Done. Updated requirements")
