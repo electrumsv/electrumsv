f308b7d9edc3f8bd872b9111f0c9a66f5ab7fa2a
rt121212121 <roger.taylor.email@gmail.com>
2021-02-23 17:13:57 +1300

    Remove dead code related to new Electrum mnemonics.

--- electrumsv/tests/test_mnemonic.py
+++ electrumsv/tests/test_mnemonic.py
@@ -7,29 +7,6 @@ from electrumsv.util import bh2u
 
 
 
-def _mnemonic_encode(i):
-    wordlist = Wordlists.bip39_wordlist("english.txt")
-    n = len(wordlist)
-    words = []
-    while i:
-        x = i%n
-        i = i//n
-        words.append(wordlist[x])
-    return ' '.join(words)
-
-
-def _mnemonic_decode(seed):
-    wordlist = Wordlists.bip39_wordlist("english.txt")
-    n = len(wordlist)
-    words = seed.split()
-    i = 0
-    while words:
-        w = words.pop()
-        k = wordlist.index(w)
-        i = i*n + k
-    return i
-
-
 class Test_NewMnemonic(unittest.TestCase):
 
     def test_to_seed(self):
@@ -37,13 +14,6 @@ class Test_NewMnemonic(unittest.TestCase):
         self.assertEqual(bh2u(seed),
                           '741b72fd15effece6bfe5a26a52184f66811bd2be363190e07a42cca442b1a5b'
                           'b22b3ad0eb338197287e6d314866c7fba863ac65d3f156087a5052ebc7157fce')
-
-    def test_random_seeds(self):
-        iters = 10
-        for _ in range(iters):
-            seed = ElectrumMnemonic.generate_new(Wordlists.bip39_wordlist("english.txt"))
-            i = _mnemonic_decode(seed)
-            self.assertEqual(_mnemonic_encode(i), seed)
 
 
 class Test_OldMnemonic(unittest.TestCase):
