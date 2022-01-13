e70d5454827dbeb91e0f37ef0c902f17b606e014
AustEcon <AustEcon0922@gmail.com>
2022-01-13 13:26:33 +1300

    LateHeaderWorkerState updates
    - Do NOT add to the state.block_transactions if we are able to process the merkle proof from the queue (i.e. we do have the matching header)
    - Refactor so that we can call `_process_one_merkle_proof` to process a single merkle proof as the roll in from the `late_header_worker_queue`.

--- electrumsv/wallet_support/late_header_worker.py
+++ electrumsv/wallet_support/late_header_worker.py
@@ -37,6 +37,7 @@
 from __future__ import annotations
 import asyncio
 import dataclasses
+import enum
 from collections import defaultdict
 from functools import partial
 from typing import Callable, cast, Tuple, Dict, Set
@@ -54,6 +55,21 @@ from ..wallet_database.functions import AsynchronousFunctions
 logger = logs.get_logger("late-header-worker")
 
 block_transactions_factory = cast(Callable[[], Dict[bytes, Set[bytes]]], partial(defaultdict, set))
+
+
+class MerkleProofErrorType(enum.IntEnum):
+    NO_PROOF_DATA = 1
+    TX_STATE_MUTATED = 2
+    BLOCK_HASH_MUTATED = 3
+    CALLBACK_DEAD = 4
+    DB_UPDATE_FAILED = 5
+    BAD_SERVER = 6
+
+
+class MerkleProofProcessingError(Exception):
+
+    def __init__(self, error_type: MerkleProofErrorType):
+        self.error_type = error_type
 
 
 @dataclasses.dataclass
@@ -88,12 +104,14 @@ async def late_header_worker_async(db_functions_async: AsynchronousFunctions,
 
 async def _populate_initial_state(db_functions_async: AsynchronousFunctions,
         state: LateHeaderWorkerState) -> None:
+    """On initial startup, the global Network singleton will sync the longest chain. This happens
+    chronologically before the Wallet instance is started (and therefore this task). Therefore,
+    we do an initial push of all transactions that are waiting for a header."""
     rows = await db_functions_async.read_pending_header_transactions_async()
     for tx_hash, block_hash, proof_data in rows:
         # When we set the proof data on a transaction for deferred verification we also set the
         # block hash among other things. This is guaranteed to be set, so essential to check.
         assert block_hash is not None
-        state.block_transactions[block_hash].add(tx_hash)
         msg = (PendingHeaderWorkKind.MERKLE_PROOF, TSCMerkleProof.from_bytes(proof_data))
         state.late_header_worker_queue.put_nowait(msg)
 
@@ -115,94 +133,123 @@ async def _process_one_item(db_functions_async: AsynchronousFunctions,
         raise NotImplementedError(f"Unknown late header work item kind {item_kind}")
 
 
+def get_confirmations_or_handle_orphan(proof_chain: Chain, proof_header: Header) -> int:
+    # TODO(1.4.0) Make a final decision what to do in this edge case where the chain of the
+    #     header is not the longest chain. This would mean the source of the proof is or was
+    #     on the reorged chain (or has reorged away from our longest chain). We might want
+    #     to do none of this and do something else.
+    assert app_state.headers is not None
+    longest_chain = cast(Chain, app_state.headers.longest_chain())
+    if proof_chain is longest_chain:
+        return cast(int, longest_chain.height - proof_header.height)
+    return 0
+
+
 async def _process_merkle_proof(db_functions_async: AsynchronousFunctions,
         state: LateHeaderWorkerState, tsc_proof: TSCMerkleProof) -> None:
+    """Process a single merkle proof or if we need to wait for the header, add it to the
+    state.block_transactions cache for re-checking for each new tip notification."""
     assert app_state.headers is not None
     assert tsc_proof.block_hash is not None
-    # Do we have the header? If so process it, otherwise record it til the header comes.
+    assert tsc_proof.transaction_hash is not None
     header: Header
     chain: Chain
     try:
         header, chain = app_state.headers.lookup(tsc_proof.block_hash)
     except MissingHeader:
-        # We will fetch the proof from the database for matching to later header arrivals.
-        # As we have confirmed that at this point the header is not present, monitor it.
-        assert tsc_proof.transaction_hash is not None
+        # We have confirmed that at this point the header is not present, monitor it.
         state.block_transactions[tsc_proof.block_hash].add(tsc_proof.transaction_hash)
-    else:
-        # Process all deferred transaction processing for the header's block.
-        assert tsc_proof.transaction_hash is not None
-        state.block_transactions[tsc_proof.block_hash].add(tsc_proof.transaction_hash)
-        await _process_block_transactions(db_functions_async, state, header, chain)
+        return None
+
+    confirmations = get_confirmations_or_handle_orphan(chain, header)
+    unverified_entries = await db_functions_async.read_transaction_proof_data_async(
+        [tsc_proof.transaction_hash])
+    tx_hash, flags, tx_block_hash, proof_data = unverified_entries[0]
+    try:
+        assert tx_block_hash is not None
+        assert proof_data is not None
+        await _process_one_merkle_proof(db_functions_async, state, tx_block_hash, flags,
+            tx_block_hash, proof_data, confirmations, header)
+    except MerkleProofProcessingError as e:
+        if e.error_type == MerkleProofErrorType.BAD_SERVER:
+            raise  # crash  # TODO(1.4.0) Handle all error types
 
 
 async def _process_header(db_functions_async: AsynchronousFunctions,
         state: LateHeaderWorkerState, header: Header, chain: Chain) -> None:
+    """Process merkle proofs in a batch-wise fashion for the given block hash"""
     block_hash = header.hash
     if block_hash in state.block_transactions:
         await _process_block_transactions(db_functions_async, state, header, chain)
 
 
+async def _process_one_merkle_proof(db_functions_async: AsynchronousFunctions,
+        state: LateHeaderWorkerState, tx_hash: bytes, flags: TxFlags, tx_block_hash: bytes,
+        proof_data: bytes, confirmations: int, header: Header) -> None:
+    """raises `MerkleProofProcessingError` with an associated enum MerkleProofErrorType
+    enumerating the possible failure cases. These errors should be recoverable"""
+    # It is possible that the transaction may have changed from under us in the database,
+    # we need to check that it hasn't, but if it has skip the transaction.
+    if proof_data is None:
+        logger.error("Deferred verification transaction %s block hash now lacks proof data",
+            hash_to_hex_str(tx_hash))
+        raise MerkleProofProcessingError(MerkleProofErrorType.NO_PROOF_DATA)
+
+    if flags & TxFlags.MASK_STATE != TxFlags.STATE_CLEARED:
+        logger.error("Deferred verification transaction %s state unexpectedly changed "
+                     "from %r to %r", hash_to_hex_str(tx_hash), TxFlags.STATE_CLEARED,
+            TxFlags(flags))
+        raise MerkleProofProcessingError(MerkleProofErrorType.TX_STATE_MUTATED)
+
+    if tx_block_hash != header.hash:
+        logger.error("Deferred verification transaction %s block hash unexpectedly changed "
+                     "from %s to %s", hash_to_hex_str(tx_hash), hash_to_hex_str(header.hash),
+            hash_to_hex_str(tx_block_hash))
+        raise MerkleProofProcessingError(MerkleProofErrorType.BLOCK_HASH_MUTATED)
+
+    tsc_proof = TSCMerkleProof.from_bytes(proof_data)
+    if verify_proof(tsc_proof, header.merkle_root):
+        if await db_functions_async.update_transaction_flags_async(tx_hash,
+                TxFlags.STATE_SETTLED, ~TxFlags.MASK_STATE):
+            callback = state.verification_callback()
+            if callback is None:
+                logger.error("Deferred verification transaction %s callback dead",
+                    hash_to_hex_str(tx_hash))
+                raise MerkleProofProcessingError(MerkleProofErrorType.CALLBACK_DEAD)
+            callback('transaction_verified', tx_hash, header, tsc_proof)
+            return None
+
+        logger.error("Deferred verification failed updating transaction %s state "
+            "from %r to %r", hash_to_hex_str(tx_hash), TxFlags(flags), TxFlags.STATE_SETTLED)
+        raise MerkleProofProcessingError(MerkleProofErrorType.DB_UPDATE_FAILED)
+    else:
+        # TODO(bad-server)
+        # TODO(1.4.0) We probably want to know what server this came from so we can treat
+        #    it as a bad server. And we would want to retry with a good server.
+        logger.error("Deferred verification transaction %s failed verifying proof",
+            hash_to_hex_str(tx_hash))
+        # Remove the "pending verification" proof and block data from the transaction, it
+        # should not be necessary to update the UI as the transaction should not have
+        # changed state and we do not display "pending verification" proofs.
+        await db_functions_async.update_transaction_proof_async(tx_hash, None, None, None,
+            TxFlags.STATE_CLEARED)
+        raise MerkleProofProcessingError(MerkleProofErrorType.BAD_SERVER)
+
+
 async def _process_block_transactions(db_functions_async: AsynchronousFunctions,
         state: LateHeaderWorkerState, header: Header, chain: Chain) -> None:
-    assert app_state.headers is not None
-    longest_chain = cast(Chain, app_state.headers.longest_chain())
-    if chain is longest_chain:
-        confirmations = longest_chain.height - header.height
-    else:
-        # TODO(1.4.0) Make a final decision what to do in this edge case where the chain of the
-        #     header is not the longest chain. This would mean the source of the proof is or was
-        #     on the reorged chain (or has reorged away from our longest chain). We might want
-        #     to do none of this and do something else.
-        confirmations = 0
-
+    confirmations = get_confirmations_or_handle_orphan(chain, header)
     tx_hashes = list(state.block_transactions[header.hash])
-    for tx_hash, flags, tx_block_hash, proof_data in \
-            await db_functions_async.read_transaction_proof_data_async(tx_hashes):
-        # It is possible that the transaction may have changed from under us in the database,
-        # we need to check that it hasn't, but if it has skip the transaction.
-        if proof_data is None:
-            logger.error("Deferred verification transaction %s block hash now lacks proof data",
-                hash_to_hex_str(tx_hash))
-            continue
-
-        if flags & TxFlags.MASK_STATE != TxFlags.STATE_CLEARED:
-            logger.error("Deferred verification transaction %s state unexpectedly changed "
-                "from %r to %r", hash_to_hex_str(tx_hash), TxFlags.STATE_CLEARED,
-                TxFlags(flags))
-            continue
-
-        if tx_block_hash != header.hash:
-            logger.error("Deferred verification transaction %s block hash unexpectedly changed "
-                "from %s to %s", hash_to_hex_str(tx_hash), hash_to_hex_str(header.hash),
-                hash_to_hex_str(tx_block_hash))
-            continue
-
-        tsc_proof = TSCMerkleProof.from_bytes(proof_data)
-        if verify_proof(tsc_proof, header.merkle_root):
-            if await db_functions_async.update_transaction_flags_async(tx_hash,
-                    TxFlags.STATE_SETTLED, ~TxFlags.MASK_STATE):
-                callback = state.verification_callback()
-                if callback is None:
-                    logger.error("Deferred verification transaction %s callback dead",
-                        hash_to_hex_str(tx_hash))
-                    break
-                callback('transaction_verified', tx_hash, header, tsc_proof)
-            else:
-                logger.error("Deferred verification failed updating transaction %s state "
-                    "from %r to %r", hash_to_hex_str(tx_hash), TxFlags(flags),
-                    TxFlags.STATE_SETTLED)
-        else:
-            # TODO(bad-server)
-            # TODO(1.4.0) We probably want to know what server this came from so we can treat
-            #    it as a bad server. And we would want to retry with a good server.
-            logger.error("Deferred verification transaction %s failed verifying proof",
-                hash_to_hex_str(tx_hash))
-            # Remove the "pending verification" proof and block data from the transaction, it
-            # should not be necessary to update the UI as the transaction should not have
-            # changed state and we do not display "pending verification" proofs.
-            await db_functions_async.update_transaction_proof_async(tx_hash, None, None, None,
-                TxFlags.STATE_CLEARED)
+    unverified_entries = await db_functions_async.read_transaction_proof_data_async(tx_hashes)
+    for tx_hash, flags, tx_block_hash, proof_data in unverified_entries:
+        try:
+            assert tx_block_hash is not None
+            assert proof_data is not None
+            await _process_one_merkle_proof(db_functions_async, state, tx_hash, flags,
+                tx_block_hash, proof_data, confirmations, header)
+        except MerkleProofProcessingError as e:
+            if e.error_type == MerkleProofErrorType.BAD_SERVER:
+                raise  # crash
+            # TODO(1.4.0) Handle all error types
 
     del state.block_transactions[header.hash]
-
