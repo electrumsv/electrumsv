06fad473cae05a8a8cb4a4e0f1706e7a88fb4425
Roger Taylor <roger.taylor.email@gmail.com>
2023-05-03 13:01:15 +1200

    MacOS build changes to reflect Apple's move away from `altool` for notarisation.

--- contrib/osx/notarize_app.sh
+++ contrib/osx/notarize_app.sh
@@ -26,57 +26,17 @@ ditto -c -k --rsrc --keepParent "$NOTARISATION_FILE" "${NOTARISATION_FILE}.zip"
 
 # Submit for notarization
 echo "Submitting $NOTARISATION_FILE for notarization..."
-RESULT=$(xcrun altool --notarize-app --type osx \
-  --file "${NOTARISATION_FILE}.zip" \
-  --primary-bundle-id io.electrumsv.electrumsv \
+xcrun notarytool submit \
   --username $APPLE_ID_USER \
   --password @env:APPLE_ID_PASSWORD \
-  --asc-provider $APPLE_ID_PROVIDER \
-  --output-format xml)
+  --team-id $APPLE_ID_PROVIDER
+  "${NOTARISATION_FILE}.zip" \
+  --wait
 
 if [ $? -ne 0 ]; then
   echo "Submitting $NOTARISATION_FILE failed:"
-  echo "$RESULT"
   exit 1
 fi
-
-REQUEST_UUID=$(echo "$RESULT" | xpath -e \
-  "//key[normalize-space(text()) = 'RequestUUID']/following-sibling::string[1]/text()")
-
-if [ -z "$REQUEST_UUID" ]; then
-  echo "Submitting $NOTARISATION_FILE failed:"
-  echo "$RESULT"
-  exit 1
-fi
-
-echo "$(echo "$RESULT" | xpath -e \
-  "//key[normalize-space(text()) = 'success-message']/following-sibling::string[1]/text()" 2> /dev/null)"
-
-# Poll for notarization status
-echo "Submitted notarization request $REQUEST_UUID, waiting for response..."
-sleep 60
-while :
-do
-  RESULT=$(xcrun altool --notarization-info "$REQUEST_UUID" \
-    --username "$APPLE_ID_USER" \
-    --password @env:APPLE_ID_PASSWORD \
-    --asc-provider $APPLE_ID_PROVIDER \
-    --output-format xml)
-  STATUS=$(echo "$RESULT" | xpath -e \
-    "//key[normalize-space(text()) = 'Status']/following-sibling::string[1]/text()" 2> /dev/null)
-
-  if [ "$STATUS" = "success" ]; then
-    echo "Notarization of $NOTARISATION_FILE succeeded!"
-    break
-  elif [ "$STATUS" = "in progress" ]; then
-    echo "Notarization in progress..."
-    sleep 20
-  else
-    echo "Notarization of $NOTARISATION_FILE failed:"
-    echo "$RESULT"
-    exit 1
-  fi
-done
 
 # Staple the notary ticket
 xcrun stapler staple "$NOTARISATION_FILE"
--- contrib/osx/notarize_file.sh
+++ contrib/osx/notarize_file.sh
@@ -22,57 +22,17 @@ cd "$NOTARISATION_FILE_DIR" || exit 1
 
 # Submit for notarization
 echo "Submitting $NOTARISATION_FILE for notarization..."
-RESULT=$(xcrun altool --notarize-app --type osx \
-  --file "${NOTARISATION_FILE}" \
-  --primary-bundle-id io.electrumsv.electrumsv \
-  --username $APPLE_ID_USER \
-  --password @env:APPLE_ID_PASSWORD \
-  --asc-provider $APPLE_ID_PROVIDER \
-  --output-format xml)
+xcrun notarytool submit \
+  --apple-id $APPLE_ID_USER \
+  --password $APPLE_ID_PASSWORD \
+  --team-id $APPLE_ID_PROVIDER \
+  --wait \
+  "${NOTARISATION_FILE}"
 
 if [ $? -ne 0 ]; then
   echo "Submitting $NOTARISATION_FILE failed:"
-  echo "$RESULT"
   exit 1
 fi
-
-REQUEST_UUID=$(echo "$RESULT" | xpath -e \
-  "//key[normalize-space(text()) = 'RequestUUID']/following-sibling::string[1]/text()")
-
-if [ -z "$REQUEST_UUID" ]; then
-  echo "Submitting $NOTARISATION_FILE failed:"
-  echo "$RESULT"
-  exit 1
-fi
-
-echo "$(echo "$RESULT" | xpath -e \
-  "//key[normalize-space(text()) = 'success-message']/following-sibling::string[1]/text()" 2> /dev/null)"
-
-# Poll for notarization status
-echo "Submitted notarization request $REQUEST_UUID, waiting for response..."
-sleep 60
-while :
-do
-  RESULT=$(xcrun altool --notarization-info "$REQUEST_UUID" \
-    --username "$APPLE_ID_USER" \
-    --password @env:APPLE_ID_PASSWORD \
-    --asc-provider $APPLE_ID_PROVIDER \
-    --output-format xml)
-  STATUS=$(echo "$RESULT" | xpath -e \
-    "//key[normalize-space(text()) = 'Status']/following-sibling::string[1]/text()" 2> /dev/null)
-
-  if [ "$STATUS" = "success" ]; then
-    echo "Notarization of $NOTARISATION_FILE succeeded!"
-    break
-  elif [ "$STATUS" = "in progress" ]; then
-    echo "Notarization in progress..."
-    sleep 20
-  else
-    echo "Notarization of $NOTARISATION_FILE failed:"
-    echo "$RESULT"
-    exit 1
-  fi
-done
 
 # Staple the notary ticket
 xcrun stapler staple "$NOTARISATION_FILE"
