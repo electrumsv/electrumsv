bbfeea191c9116dc54f15ef9002fb664ccc26284
Roger Taylor <roger.taylor.email@gmail.com>
2023-08-28 18:28:28 +1200

    Update requirements for latest mypy. Windows only for now. CI bypasses the requirements.. though if it didn't we wouldn't have to chase pointless differences between pinned dev requirement version and the later mypy package releases.

--- electrumsv/network_support/general_api.py
+++ electrumsv/network_support/general_api.py
@@ -1293,10 +1293,7 @@ async def prepare_server_tip_filter_peer_channel(indexing_server_state: ServerCo
     settings_object = await update_server_indexer_settings(indexing_server_state,
         settings_delta_object)
 
-    # NOTE(typing) Type is incompatible with same type, who knows? Error message as follows:
-    # `Argument 1 to "update" of "TypedDict" has incompatible type "IndexerServerSettings";
-    # expected "TypedDict({'tipFilterCallbackUrl'?: str | None})"  [typeddict-item]`
-    indexing_server_state.indexer_settings.update(settings_delta_object) # type: ignore
+    indexing_server_state.indexer_settings.update(settings_delta_object)
     if settings_object != indexing_server_state.indexer_settings:
         # TODO(1.4.0) Unreliable server, issue#841. Differing server indexer settings after setup.
         #     Server unreliability case OR user unreliability with clashing wallets open using
